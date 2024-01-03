365e663982a8070de661c7853f31267539060896
sp6802 <104307369+sp6802@users.noreply.github.com>
2024-01-03 23:13:24 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,77 @@
+
+
+let allMusic = [
+  {
+    name: "Tiroteo - Remix",
+    artist: "Marc Seguí, Rauw Alejandro, Pol Granch",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Va Va Voom",
+    artist: "Tejas, Shankar-Ehsaan-Loy, Javed Akhtar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Sunoh",
+    artist: "Tejas, Javed Akhtar, Ankur Tewari, The Islanders, Dot.",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Pehle Bhi Main",
+    artist: "Vishal Mishra, Raj Shekhar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Aradhya ",
+    artist: "Hesham Abdul Wahab, Jubin Nautiyal, Palak Muchhal",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "The Scientist",
+    artist: "Corinne Bailey Rae",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Love Me Like You Do",
+    artist: "Ellie Goulding",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "For You (Fifty Shades Freed) (& Rita Ora)",
+    artist: "Liam Payne, Rita Ora",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "	O Maahi (From Dunki)",
+    artist: "Pritam & Arijit Singh & Irshad Kamil",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "	Heeriye",
+    artist: "Jasleen Royal, Arijit Singh, and Harrykahanhai",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "	Laal Ishq",
+    artist: "Arijit Singh, and Siddharth",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "	Bojhena Shey Bojhena (Male)",
+    artist: "Arijit Singh",
+    img: "music-12",
+    src: "music-12"
+  },
+ 
+];
